637989bbeed10ac2a6d57b7e03f18f513b3f611b
omer.mindivanli <omer.mindivanli@gmail.com>
2018-06-19 00:23:58 +0200

    destruction and arrow function have been added

---
+++ arrowFunction.js
@@ -0,0 +1,16 @@
+// Arrow Functions
+// ES5
+let a = 2;
+
+function random() {
+  return a * a;
+}
+
+console.log(random());
+
+// ES6
+let randomES6 = () => {
+  return a * a * a;
+};
+
+console.log(randomES6());
---
+++ destruction.js
@@ -0,0 +1,25 @@
+// Destruction for Object
+// ES5
+let props = {
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+  prop4: "value4"
+};
+
+let prop1 = props.prop1;
+let prop2 = props.prop2;
+
+console.log(prop1);
+console.log(prop2);
+
+// ES6
+let { prop3, prop4 } = props;
+console.log(prop3);
+console.log(prop4);
+
+// Destruction for Array ES6
+const arr = [1, 2, 3];
+const [val1, val2, val3] = arr;
+
+console.log(arr);
